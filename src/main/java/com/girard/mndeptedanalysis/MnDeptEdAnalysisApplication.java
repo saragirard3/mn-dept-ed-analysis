0dbaee980ddb77b54f057f766a81f5aab6a40576
Sara Girard <sarag.bsa@gmail.com>
2023-09-26 06:17:42 -0400

    spring app base setup

---
+++ src/main/java/com/girard/mndeptedanalysis/MnDeptEdAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.girard.mndeptedanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnDeptEdAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnDeptEdAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/girard/mndeptedanalysis/MnDeptEdAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.girard.mndeptedanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnDeptEdAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
