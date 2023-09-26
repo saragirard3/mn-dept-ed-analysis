b0649bb7f0b0db602dc1d4156f2a0557922e0f04
Sara Girard <sarag.bsa@gmail.com>
2023-09-26 09:41:45 -0400

    create district table and import sql data

---
+++ src/main/java/com/girard/mndeptedanalysis/entity/District.java
@@ -0,0 +1,26 @@
+package com.girard.mndeptedanalysis.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "district")
+public class District {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer districtNumber;
+    private String districtName;
+
+    public District() {
+    }
+
+    public District(Integer id, Integer districtNumber, String districtName) {
+        this.id = id;
+        this.districtNumber = districtNumber;
+        this.districtName = districtName;
+    }
+
+
+}
